e4b202353ee5d4b4f18ee29c770f91e023e2f7db
benson46 <bensonbaiju46@gmail.com>
2024-10-18 19:50:04 +0530

    css completed

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -3,32 +3,81 @@ import './ToDoList.css'
 
 function ToDoList(){
 
+    const [task,setTask] = useState("");
+
+    const [tasks,setTasks] = useState([]);
+
+
+    const newTask = (event)=>{
+        setTask(event.target.value)
+    }
+
+    const addTask = ()=>{
+        if(task.trim() !== ""){
+            setTasks(prevTasks => [...prevTasks, {text:task,checkedValue:false,flag:false}]);
+            setTask("")
+        }
+    }
+
+    const checkMarkCheck = (index)=>{
+        setTasks(prevTasks =>
+            prevTasks.map((task, i) =>
+                i === index ? { ...task, checkedValue: !task.checkedValue } : task
+            )
+        );
+    };
+
+
+
+    const editTask = (index) => {
+        setTasks(prevTasks =>
+            prevTasks.map((task, i) =>
+                i === index ? { ...task, flag: !task.flag } : task
+            )
+        );
+    };
+
+    const changeTask = (event,index) =>{
+       const update = event.target.value;
+       setTasks(prevTasks => 
+        prevTasks.map((task,i)=>
+        i === index? {...task,text:update} : task
+        ))
+    }
 
     
 
     return(
         <div className='body-todo'>
-        <h1>To-Do List</h1>
+            <h1>To-Do List</h1>
 
-        <input  type="text" placeholder='Enter New Task To Do' />
-        <button className='add-button'>Add</button>
-        <div>
-            <ol>
-                <li>
-                    
-                    <input type="checkbox" />
-                    <p>taskdksssssssssssskadssssssssadjfffffffffffffffffffffff</p>
+            <input  type="text" placeholder='Enter New Task To Do' value={task} onChange={newTask}/>
+            <button className='add-button' type='button' onClick={addTask}>Add</button>
+            <div>
+                <ol>
+                    {tasks.map((task,index) =>(
 
-                    <div className='li-buttons'>
-                        <button className='edit-button'>EDIT</button>
-                        <button className='up-button'>UP</button>
-                        <button className='down-button'>DOWN</button>
-                        <button className='delete-button'>DELETE</button>
-                    </div>
+                        <li key={index}>
+                            <input type="checkbox" checked={task.checkedValue}  onChange={()=>checkMarkCheck(index)}/>
+                            {task.flag === true ? (<p>{task.text}</p>) : <input type="text" value={task.text} onChange={(event)=>changeTask(event,index)} />}
+                            
 
-                </li>
-            </ol>
-        </div>
+                            <div className='li-buttons'>
+                                {!task.checkedValue ? (
+                                    <>
+                                    {task.flag === true ? <button className='edit-button' onClick={()=>editTask(index)}>EDIT</button> : <button className='edit-button' onClick={()=>editTask(index)}>SAVE</button> }
+                                        
+                                        <button className='up-button'>UP</button>
+                                        <button className='down-button'>DOWN</button>
+                                        <button className='delete-button'>DELETE</button>
+                                    </>
+                                ):"completed"}
+                            </div>
+                        </li>  
+
+                    ))}
+                </ol>
+            </div>
         </div>
     )
 }
